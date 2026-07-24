7c61bfe27acc561780fa587efc2c1daf4dc7dea1
Andrew <151686297+andrew-somerset@users.noreply.github.com>
2026-07-24 14:27:02 -0400

    Add: design-linked-list - submission-0

---
+++ Data Structures & Algorithms/design-linked-list/submission-0.py
@@ -0,0 +1,57 @@
+class ListNode:
+    def __init__(self,val):
+        self.val = val
+        self.next = None
+        self.prev = None
+
+class MyLinkedList:
+
+    def __init__(self):
+        self.left = ListNode(0)
+        self.right = ListNode(0)
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    def get(self, index: int) -> int:
+        cur = self.left.next
+        while cur and index > 0:
+            cur = cur.next
+            index -= 1
+        if cur and cur != self.right and index == 0:
+            return cur.val
+        return -1
+
+    def addAtHead(self, val: int) -> None:
+        node = ListNode(val)
+        node.next = self.left.next
+        node.prev = self.left
+        self.left.next.prev = node
+        self.left.next = node
+
+    def addAtTail(self, val: int) -> None:
+        node = ListNode(val)
+        node.next = self.right
+        node.prev = self.right.prev
+        self.right.prev.next = node
+        self.right.prev = node
+        
+    def addAtIndex(self, index: int, val: int) -> None:
+        cur = self.left.next
+        while cur and index > 0:
+            cur = cur.next
+            index -= 1
+        if cur and index == 0:
+            node = ListNode(val)
+            node.next = cur
+            node.prev = cur.prev
+            cur.prev.next = node
+            cur.prev = node
+
+    def deleteAtIndex(self, index: int) -> None:
+        cur = self.left.next
+        while cur and index > 0:
+            cur = cur.next
+            index -= 1
+        if cur and cur != self.right and index == 0:
+            cur.prev.next = cur.next
+            cur.next.prev = cur.prev
